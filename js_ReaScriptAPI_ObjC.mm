ef20487127d1d79a594d1f790736832a007429d0
juliansader <julian.sader@gmail.com>
2018-10-16 19:04:54 +0200

    [skip ci]

--- js_ReaScriptAPI_ObjC.mm
+++ js_ReaScriptAPI_ObjC.mm
@@ -1,8 +1,7 @@
 #import <Cocoa/Cocoa.h>
 #import <objc/objc-runtime.h>
-#include "swell.h"
 
-void JS_Window_SetTransparency_ObjC(HWND hwnd, double alpha)
+void JS_Window_SetTransparency_ObjC(void* hwnd, double alpha)
 {
    if ([(id)hwnd isKindOfClass:[NSWindow class]])
    {
