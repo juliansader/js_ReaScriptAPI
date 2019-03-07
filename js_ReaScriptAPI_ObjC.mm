bb5d1bc6f0c98a4f678ee547d1a2087d646ac5aa
juliansader <julian.sader@gmail.com>
2019-03-07 08:37:51 +0200

    [skip ci]

--- js_ReaScriptAPI_ObjC.mm
+++ js_ReaScriptAPI_ObjC.mm
@@ -1,7 +1,7 @@
 #import <Cocoa/Cocoa.h>
 #import <objc/objc-runtime.h>
 
-int JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
+int JS_Window_SetOpacity_ObjC(HWND hwnd, double alpha)
 {
    if ([(id)hwnd isKindOfClass:[NSWindow class]])
    {
