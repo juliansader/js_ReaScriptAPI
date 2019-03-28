1ac1d805fe4d0293e0fe022b6f96fcad4cf3b483
juliansader <julian.sader@gmail.com>
2019-03-28 16:00:32 +0200

    2

--- js_ReaScriptAPI_vararg.h
+++ js_ReaScriptAPI_vararg.h
@@ -17,3 +17,28 @@ static void* __vararg_JS_Localize(void** arglist, int numparms)
 	return NULL;
 }
 
+//////////////////////////////////////////////////////////////////////////////////
+
+static void* __vararg_JS_VKeys_GetState(void** arglist, int numparms)
+{
+	return (void*)(intptr_t)JS_VKeys_GetState((char*)arglist[0], (int)(intptr_t)arglist[1]);
+}
+
+static void* __vararg_JS_VKeys_GetHistory(void** arglist, int numparms)
+{
+	return (void*)(intptr_t)JS_VKeys_GetHistory((char*)arglist[0], (int)(intptr_t)arglist[1]);
+}
+
+static void* __vararg_JS_VKeys_ClearHistory(void** arglist, int numparms)
+{
+	JS_VKeys_ClearHistory();
+	return nullptr;
+}
+
+static void* __vararg_JS_VKeys_Intercept(void** arglist, int numparms)
+{
+	return (void*)(intptr_t)JS_VKeys_Intercept((int)(intptr_t)arglist[0], (int)(intptr_t)arglist[1]);
+}
+
+////////////////////////////////////////////////////////////////////////////
+
