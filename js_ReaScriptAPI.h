f2a24bb15a7a0d0b5710ed828c7c0222d22c488b
juliansader <julian.sader@gmail.com>
2019-03-28 15:59:01 +0200

    2 [skip ci]

--- js_ReaScriptAPI.h
+++ js_ReaScriptAPI.h
@@ -5,3 +5,9 @@ extern "C" REAPER_PLUGIN_DLL_EXPORT int REAPER_PLUGIN_ENTRYPOINT(REAPER_PLUGIN_H
 void  JS_ReaScriptAPI_Version(double* versionOut);
 
 void  JS_Localize(const char* USEnglish, const char* LangPackSection, char* translationOut, int translationOut_sz);
+
+int   JS_VKeys_Callback(MSG* event, accelerator_register_t*);
+bool  JS_VKeys_GetState(char* stateOutNeedBig, int stateOutNeedBig_sz);
+void  JS_VKeys_ClearHistory();
+bool  JS_VKeys_GetHistory(char* stateOutNeedBig, int stateOutNeedBig_sz);
+int   JS_VKeys_Intercept(int keyCode, int intercept);
