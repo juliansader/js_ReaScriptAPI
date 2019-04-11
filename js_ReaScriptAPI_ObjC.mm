ed184b4b96958774154a3c9b0cc6098e49338010
juliansader <julian.sader@gmail.com>
2019-04-11 12:41:23 +0200

    v0.982 opacity

--- js_ReaScriptAPI_ObjC.mm
+++ js_ReaScriptAPI_ObjC.mm
@@ -3,14 +3,14 @@
 
 bool JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
 {
-   if ([(id)hwnd isKindOfClass:[NSWindow class]])
-   {
+   //if ([(id)hwnd isKindOfClass:[NSWindow class]])
+   //{
       NSWindow *window = (NSWindow *)hwnd;
       CGFloat opacity = alpha;
       [window setAlphaValue:opacity];
       [window setOpaque:NO];
 	  return true;
-   }  
-   else
-		return false;
+   //}  
+   //else
+//		return false;
 }
