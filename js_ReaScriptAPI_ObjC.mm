72ff78b67b9e9ebaf69c2829c4e501e2119e6c10
juliansader <julian.sader@gmail.com>
2019-06-14 08:36:10 +0200

    v0.989 OSX transparency test

--- js_ReaScriptAPI_ObjC.mm
+++ js_ReaScriptAPI_ObjC.mm
@@ -6,11 +6,14 @@ bool JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
    // For some reason, isKindOfClass doesn't work
    //if ([(id)hwnd isKindOfClass:[NSWindow class]])
    //{
-      NSWindow* window = (NSWindow*)hwnd;
-      CGFloat opacity = (CGFloat) alpha;
-      [window setAlphaValue:opacity];
-      [window setOpaque:NO];
-	  return true;
+      	NSWindow* window = (NSWindow*)hwnd;
+      	CGFloat opacity = (CGFloat)alpha;
+	[window setOpaque:NO];
+	[window setWantsLayer:YES];
+      	[window setAlphaValue:opacity];
+	[window setBackgroundColor:[NSColor clearColor]];
+	[window.layer setBackgroundColor:[NSColor clearColor]];
+	return true;
    //}  
    //else
    // 	return false;
