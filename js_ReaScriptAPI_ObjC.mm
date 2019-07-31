757487ecf16de7ca8d6fdb4b536c6ff2ab716038
juliansader <julian.sader@gmail.com>
2019-07-31 23:38:12 +0200

    Test NSView, set background and layer

--- js_ReaScriptAPI_ObjC.mm
+++ js_ReaScriptAPI_ObjC.mm
@@ -3,7 +3,6 @@
 
 bool JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
 {
-   // For some reason, isKindOfClass doesn't work
    if ([hwnd isKindOfClass:[NSView class]])
    {
       	NSView* view = (NSView*)hwnd;
@@ -12,9 +11,9 @@ bool JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
 	{
 		CGFloat opacity = (CGFloat)alpha;
 		[window setOpaque:NO];
-		//[window setWantsLayer:YES];
+		[window setWantsLayer:YES];
 		[window setAlphaValue:opacity];
-		//[window setBackgroundColor:[NSColor clearColor]];
+		[window setBackgroundColor:[NSColor clearColor]];
 		//[window.layer setBackgroundColor:[NSColor clearColor]];
 		return true;
 	}
