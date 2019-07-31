0c518cfd27c9e607d81452a9aaeb880c32e6f4a4
juliansader <julian.sader@gmail.com>
2019-07-31 23:12:46 +0200

    Try getting NSWindow 1

--- js_ReaScriptAPI_ObjC.mm
+++ js_ReaScriptAPI_ObjC.mm
@@ -6,14 +6,20 @@ bool JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
    // For some reason, isKindOfClass doesn't work
    //if ([(id)hwnd isKindOfClass:[NSWindow class]])
    //{
-      	NSWindow* window = (NSWindow*)hwnd;
-      	CGFloat opacity = (CGFloat)alpha;
-	[window setOpaque:NO];
-	[window setWantsLayer:YES];
-      	[window setAlphaValue:opacity];
-	[window setBackgroundColor:[NSColor clearColor]];
-	//[window.layer setBackgroundColor:[NSColor clearColor]];
-	return true;
+      	NSView* view = (NSView*)hwnd;
+	NSWindow* window = (NSWindow*)[view window];
+	if (window)
+	{
+		CGFloat opacity = (CGFloat)alpha;
+		[window setOpaque:NO];
+		//[window setWantsLayer:YES];
+		[window setAlphaValue:opacity];
+		[window setBackgroundColor:[NSColor clearColor]];
+		//[window.layer setBackgroundColor:[NSColor clearColor]];
+		return true;
+	}
+	else
+		return false;
    //}  
    //else
    // 	return false;
