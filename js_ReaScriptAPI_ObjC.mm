211367925fc867cb1bd3f3bc2e84257747e768db
juliansader <julian.sader@gmail.com>
2019-07-31 23:57:44 +0200

    Test NSView, don't change background [skip ci]

--- js_ReaScriptAPI_ObjC.mm
+++ js_ReaScriptAPI_ObjC.mm
@@ -6,14 +6,14 @@ bool JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
    if ([hwnd isKindOfClass:[NSView class]])
    {
       	NSView* view = (NSView*)hwnd;
-	NSWindow* window = (NSWindow*)[view window];
+	NSWindow* window = (NSWindow*)[view window]; // The view’s window object, if it is installed in a window.
 	if (window)
 	{
 		CGFloat opacity = (CGFloat)alpha;
 		[window setOpaque:NO];
-		[window setWantsLayer:YES];
+		//[window setWantsLayer:YES];
 		[window setAlphaValue:opacity];
-		[window setBackgroundColor:[NSColor clearColor]];
+		//[window setBackgroundColor:[NSColor clearColor]];
 		//[window.layer setBackgroundColor:[NSColor clearColor]];
 		return true;
 	}
