8c57e0fdb52d083ff145cd4a7aa07bac5a617ed3
juliansader <julian.sader@gmail.com>
2019-07-31 23:31:23 +0200

    test NSView, don't change background

--- js_ReaScriptAPI_ObjC.mm
+++ js_ReaScriptAPI_ObjC.mm
@@ -4,8 +4,8 @@
 bool JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
 {
    // For some reason, isKindOfClass doesn't work
-   //if ([(id)hwnd isKindOfClass:[NSWindow class]])
-   //{
+   if ([hwnd isKindOfClass:[NSView class]])
+   {
       	NSView* view = (NSView*)hwnd;
 	NSWindow* window = (NSWindow*)[view window];
 	if (window)
@@ -14,13 +14,10 @@ bool JS_Window_SetOpacity_ObjC(void* hwnd, double alpha)
 		[window setOpaque:NO];
 		//[window setWantsLayer:YES];
 		[window setAlphaValue:opacity];
-		[window setBackgroundColor:[NSColor clearColor]];
+		//[window setBackgroundColor:[NSColor clearColor]];
 		//[window.layer setBackgroundColor:[NSColor clearColor]];
 		return true;
 	}
-	else
-		return false;
-   //}  
-   //else
-   // 	return false;
+   }  
+   return false;
 }
