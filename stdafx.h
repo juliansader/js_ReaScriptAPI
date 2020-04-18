0382f94a84e95d89c8a35e1eac24b70e48ac7834
juliansader <julian.sader@gmail.com>
2020-04-18 07:37:38 +0200

    v1.002

--- stdafx.h
+++ stdafx.h
@@ -63,8 +63,8 @@
 	#include "./WDL/swell/swell-internal.h" // For definition of HWND__
 	#define WINAPI
 #else
-	//#define SWELL_TARGET_OSX
-	//#define __OBJC__
+	#define SWELL_TARGET_OSX
+	#define __OBJC__
 	#include "./WDL/swell/swell-internal.h"
 	#define WINAPI
 #endif
