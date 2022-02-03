16c9df98aa92c14dd0d2a674e10cb4e56d3b95bf
juliansader <julian.sader@gmail.com>
2022-02-03 22:54:08 +0200

    v1.301 appveyor

--- js_ReaScriptAPI.cpp
+++ js_ReaScriptAPI.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-#define JS_REASCRIPTAPI_VERSION 1.300
+#define JS_REASCRIPTAPI_VERSION 1.301
 
 #ifndef _WIN32
 #define _WDL_SWELL 1 // So that I don't have to type #ifdef __linux__ and __APPLE__ everywhere
